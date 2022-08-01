4a9d000bba04beaaea6ab74de5b3e7c8601b9c1c
Anirban Chakraborty <anirban0451@gmail.com>
2022-08-01 06:42:18 -0400

    An R code for installing all the prerequisite packages for running other codes.

---
+++ install-all-packages.R
@@ -0,0 +1,13 @@
+install.packages("igraph"); 
+install.packages("Rfast")
+install.packages("ggraph"); 
+install.packages("tidyverse"); 
+install.packages("ggplot2"); 
+install.packages("latex2exp");
+install.packages("tidygraph"); 
+install.packages("RColorBrewer"); 
+install.packages("GPvecchia")
+install.packages("VEnKF_0.9.1.tar.gz", repos = NULL, type = "source")
+install.packages("foreach")
+install.packages("iterators")
+install.packages("doParallel")
